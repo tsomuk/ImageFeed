a457f3fc2c7c391fef59301f9df4dc5cbb10f5a9
Nikita <tsomuk@me.com>
2023-07-29 22:00:11 +0300

    Profile page added. Some changes in assets

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Nikita Tsomuk on 29.07.2023.
+//
+
+import UIKit
+
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var userPickImageView: UIImageView!
+    @IBOutlet private var userNameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+   
+    @IBAction func exitButtonTaped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+}
