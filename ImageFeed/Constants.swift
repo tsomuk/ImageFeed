acbcfdd9de7b195bccc72e343a37e368e3f90e5b
Nikita <tsomuk@me.com>
2023-08-29 10:17:08 +0300

    constants file added

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Nikita Tsomuk on 29.08.2023.
+//
+
+import Foundation
+
+
+let SecretKey = "Nt04IHldFukSep8Cbvy4w2Spkp9B33bfEmcMSN5Z3zU"
+let AccessKey = "TIIkrtuIhItIuiCkXyZGRDj3K_AJNTGRguzhj-u5wmQ"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+
+
