177c5f88cc46653929afa9957d1762842df35458
Tsomuk Nikita <tsomuk@me.com>
2024-05-20 17:14:33 +0300

    add readme file

---
+++ ImageFeed/Presenters/WebViewPresenter.swift
@@ -0,0 +1,69 @@
+//
+//  WebViewPresenter.swift
+//  ImageFeed
+//
+//  Created by Nikita Tsomuk on 28/12/2023.
+//
+
+import Foundation
+
+
+public protocol WebViewPresenterProtocol {
+  var view: WebViewViewControllerProtocol? { get set }
+  func viewDidLoad()
+  func didUpdateProgressValue(_ newValue: Double)
+  func code(from url: URL) -> String?
+}
+
+final class WebViewPresenter {
+  // MARK: - Private properties
+
+  private var webViewWasLoaded = false // to prevent revers progressView animation
+
+  // MARK: - Public properties
+
+  weak var view: WebViewViewControllerProtocol?
+  var authHelper: AuthHelperProtocol
+
+  // MARK: - Init
+
+  init(authHelper: AuthHelperProtocol) {
+    self.authHelper = authHelper
+  }
+}
+
+// MARK: - Private properties & methods
+
+extension WebViewPresenter {
+  func shouldHideProgress(for value: Float) -> Bool {
+    abs(value - 1.0) <= 0.001
+  }
+}
+
+// MARK: - WebViewPresenterProtocol
+
+extension WebViewPresenter: WebViewPresenterProtocol {
+  func viewDidLoad() {
+    let request = authHelper.authRequest()
+    view?.load(request: request)
+    didUpdateProgressValue(0)
+      print("test")
+  }
+
+  func didUpdateProgressValue(_ newValue: Double) {
+    if !webViewWasLoaded {
+      let newProgressValue = Float(newValue)
+      view?.setProgressValue(newProgressValue)
+
+      let shouldProgressHidden = shouldHideProgress(for: newProgressValue)
+      view?.setProgressHidden(shouldProgressHidden)
+      if shouldProgressHidden {
+        webViewWasLoaded = true
+      }
+    }
+  }
+
+  func code(from url: URL) -> String? {
+    authHelper.code(from: url)
+  }
+}
